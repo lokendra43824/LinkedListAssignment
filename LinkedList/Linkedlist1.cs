c2b68dcd402c538298b26caed6c732d2a36a2dee
Lokendra <lokilokendra12345@gmail.com>
2020-10-21 17:15:39 +0530

    [lokendra] Added a code to uc_2_adding_nodes_at_first

--- LinkedList/Linkedlist1.cs
+++ LinkedList/Linkedlist1.cs
@@ -4,45 +4,59 @@ using System.Text;
 
 namespace dataStructures
 {
-    public class Linkedlist1
+    class LinkedList
     {
-        internal Node head;
-        internal void Add(int data)
+        public Node head;
+        public void Add(int data)
         {
             Node node = new Node(data);
-            if(this.head == null)
+            if (head == null)
             {
-                this.head = node;
+                head = node;
             }
             else
             {
                 Node temp = head;
-                while(temp.next != null)
+                while (temp.next != null)
                 {
                     temp = temp.next;
                 }
+
                 temp.next = node;
+                Console.WriteLine("{0} inserted into linked list", node.data);
+
             }
-            Console.WriteLine("{0} inserted into linked list", node.data);
+
+        }
+        public void AddFirst(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = head;
+            }
+            head = node;
+           // Console.WriteLine("{0} inserted into linked list", node.data);
         }
 
-        internal string search(int v)
+        public void Display()
         {
-            throw new NotImplementedException();
-        }
-
-        internal void Display()
-        {
-            Node temp = this.head;
-            if(temp == null)
+            if (head == null)
             {
                 Console.WriteLine("Linked list is empty");
-                return;
             }
-            while(temp != null)
+            else
             {
-                Console.Write(temp.data + " ");
-                temp = temp.next;
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
             }
         }
     }
